27ceea8566a38f569c091fe504ff78ada4bc36cd
cheykoff <mail@michael-schoelkopf.de>
2023-03-29 16:54:29 +0200

    ux: use material design in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms'; // import the ReactiveFormsModule
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { QuestionCardComponent } from './components/question-card/question-card.component';
+import { SelectTopicComponent } from './components/select-topic/select-topic.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionCardComponent, SelectTopicComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     BrowserAnimationsModule, // add the ReactiveFormsModule to the imports array
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/question-card/question-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-question-card',
+  templateUrl: './question-card.component.html',
+  styleUrls: ['./question-card.component.scss'],
+})
+export class QuestionCardComponent {}
